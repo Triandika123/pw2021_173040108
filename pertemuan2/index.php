cb5ba4fe306a078b1def5c5c69ff16ed7870bd97
Triandika <173040108@mail.unpas.ac.id>
2021-02-10 11:24:18 +0700

    menyelesaikan tugas pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Triandika Ramadhan
+173040108
+https://github.com/Triandika123/pw2021_173040108
+pertemuan 2 - 8 februari 2021
+
+mempelajari tentang sintaks PHP
+*/
+?>
+<?php
+// STandar Output
+//echo, print
+?>
